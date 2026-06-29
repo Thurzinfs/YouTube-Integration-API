20f96d465ad621a6752236c91d82ef1c15f74fcf
Thurzinfs <francasilvaarthur95@gmail.com>
2026-06-29 12:22:50 -0300

    feat: implementacao e finalizacao do container responsavel pela injecao de dependencia

--- app/accounts/api/dependencies.py
+++ app/accounts/api/dependencies.py
@@ -1,6 +1,12 @@
 from dependency_injector import containers, providers
 
-from app.accounts.application.use_cases import DeactiveUserUseCase, RegisterUserUseCase, ResponseUserByEmailUseCase, ResponseUserByIdUseCase, UpdateUserUseCase
+from app.accounts.application.use_cases import (
+    DeactiveUserUseCase,
+    RegisterUserUseCase,
+    ResponseUserByEmailUseCase,
+    ResponseUserByIdUseCase,
+    UpdateUserUseCase,
+)
 from app.accounts.infrastructure.repository import UserRepository
 from app.accounts.infrastructure.services import HashService
 
@@ -11,28 +17,21 @@ class AccountContainer(containers.DeclarativeContainer):
     hash_service = providers.Factory(HashService)
 
     register_user_use_case = providers.Factory(
-        RegisterUserUseCase,
-        user_repo=user_repo,
-        hash_service=hash_service
+        RegisterUserUseCase, user_repo=user_repo, hash_service=hash_service
     )
 
     response_user_by_id_user_case = providers.Factory(
-        ResponseUserByIdUseCase,
-        user_repo=user_repo
+        ResponseUserByIdUseCase, user_repo=user_repo
     )
 
     response_user_by_email_user_case = providers.Factory(
-        ResponseUserByEmailUseCase,
-        user_repo=user_repo
+        ResponseUserByEmailUseCase, user_repo=user_repo
     )
 
     update_user_use_case = providers.Factory(
-        UpdateUserUseCase,
-        user_repo=user_repo,
-        hash_service=hash_service
+        UpdateUserUseCase, user_repo=user_repo, hash_service=hash_service
     )
 
     deactive_user_use_case = providers.Factory(
-        DeactiveUserUseCase,
-        user_repo=user_repo
+        DeactiveUserUseCase, user_repo=user_repo
     )

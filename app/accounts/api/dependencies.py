be5f6d356536ab34882a8567b45f20a8ec21b0b5
Thurzinfs <francasilvaarthur95@gmail.com>
2026-06-28 21:40:27 -0300

    feat: implementando injecao de dependencias

---
+++ app/accounts/api/dependencies.py
@@ -0,0 +1,38 @@
+from dependency_injector import containers, providers
+
+from app.accounts.application.use_cases import DeactiveUserUseCase, RegisterUserUseCase, ResponseUserByEmailUseCase, ResponseUserByIdUseCase, UpdateUserUseCase
+from app.accounts.infrastructure.repository import UserRepository
+from app.accounts.infrastructure.services import HashService
+
+
+class AccountContainer(containers.DeclarativeContainer):
+    user_repo = providers.Factory(UserRepository)
+
+    hash_service = providers.Factory(HashService)
+
+    register_user_use_case = providers.Factory(
+        RegisterUserUseCase,
+        user_repo=user_repo,
+        hash_service=hash_service
+    )
+
+    response_user_by_id_user_case = providers.Factory(
+        ResponseUserByIdUseCase,
+        user_repo=user_repo
+    )
+
+    response_user_by_email_user_case = providers.Factory(
+        ResponseUserByEmailUseCase,
+        user_repo=user_repo
+    )
+
+    update_user_use_case = providers.Factory(
+        UpdateUserUseCase,
+        user_repo=user_repo,
+        hash_service=hash_service
+    )
+
+    deactive_user_use_case = providers.Factory(
+        DeactiveUserUseCase,
+        user_repo=user_repo
+    )

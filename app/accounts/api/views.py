c5e92766ac6e533917b74834b6ab356c254b09d3
Thurzinfs <francasilvaarthur95@gmail.com>
2026-06-29 12:28:40 -0300

    feat: implementando em views as rotas

--- app/accounts/api/views.py
+++ app/accounts/api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from uuid import UUID
 
-# Create your views here.
+from ninja import Router
+from pydantic import EmailStr
+
+from app.accounts.api.dependencies import AccountContainer
+from app.accounts.api.schemas import UserIn, UserOut
+
+from django.db.transaction import atomic
+
+router = Router()
+
+container = AccountContainer()
+
+
+@router.post('/', response={201: UserOut})
+@atomic
+def create_user(request, data: UserIn):
+    dto = data.to_dto()
+
+    use_case = container.register_user_use_case()
+
+    user = use_case.execute(dto)
+
+    return UserOut.from_domain(user)
--- config/urls.py
+++ config/urls.py
@@ -3,12 +3,17 @@ from django.urls import path
 
 from ninja import NinjaAPI
 
+from app.accounts.api.views import router as account_router
+
 api = NinjaAPI(title='Music YouTube API', docs_url='/docs/')
 
 
 @api.get('/health', tags=['Health'])
 def health_check(request):
     return {'msg': 'OK'}
+
+
+api.add_router('/account', account_router, tags=['Accounts'])
 
 
 urlpatterns = [path('admin/', admin.site.urls), path('api/v1/', api.urls)]

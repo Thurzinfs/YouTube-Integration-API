6333302c0bef20ff9dc9fb12cc8c2874589c9ad5
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-05 22:36:00 -0300

    feat: implementando rotas de requisicao referente ao modulo music

--- app/music/api/views.py
+++ app/music/api/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from typing import List
+from uuid import UUID
 
-# Create your views here.
+from ninja import Router
+
+from app.music.api.dependencies import MusicContainer
+from app.music.api.schemas import MediaSourceIn, MediaSourceOut
+
+from django.db.transaction import atomic
+
+
+router = Router()
+
+container = MusicContainer()
+
+
+@router.post('/', response={201: MediaSourceOut})
+@atomic
+def register_music(request, data: MediaSourceIn):
+    dto = data.to_dto()
+
+    use_case = container.start_register_music_use_case()
+
+    music = use_case.execute(dto)
+
+    return MediaSourceOut.from_domain(music)
+
+@router.get('/{id}', response={200: MediaSourceOut})
+def response_music(request, id: UUID):
+    use_case = container.response_music_use_case()
+
+    music = use_case.execute(id)
+
+    return MediaSourceOut.from_domain(music)
+
+
+@router.get("/musicas/buscar", response=List[MediaSourceOut])
+def search_musics(request, term: str):
+    use_case = container.search_musics_by_similarity_use_case()
+
+    musics = use_case.execute(term)
+
+    return [
+        MediaSourceOut.from_domain(dto)
+        for dto in musics
+    ]
+
+
+@router.delete('/{id}', response={200: MediaSourceOut})
+@atomic
+def deactive_music(request, id: UUID):
+    use_case = container.deactive_music_use_case()
+
+    music = use_case.execute(id)
+
+    return MediaSourceOut.from_domain(music)

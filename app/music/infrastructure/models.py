5bccedc758a163babf640e6c67005dc7d99a1185
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-05 22:34:28 -0300

    feat: implementando ferramenta necessaria a busca por similaridade

--- app/music/infrastructure/models.py
+++ app/music/infrastructure/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 from app.music.domain.roles import StatusMusic
 
+from django.contrib.postgres.indexes import GinIndex
+
 
 class MediaSource(models.Model):
     """
@@ -12,15 +14,20 @@ class MediaSource(models.Model):
 
     id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
     original_url = models.URLField()
-    title = models.CharField(max_length=255)
-    channel_name = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, null=True)
+    channel_name = models.CharField(max_length=255, null=True)
     duration_seconds = models.IntegerField()
     status = models.CharField(
         max_length=40, choices=StatusMusic, default=StatusMusic.pending
     )
-    audio_file_path = models.FileField(upload_to='music/audio/', null=False)
+    audio_file_path = models.FileField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     deleted_at = models.DateTimeField(null=True, default=None)
 
     class Meta:
+        indexes = [
+            GinIndex(fields=['title'], name='music_name_trgm', opclasses=['gin_trgm_ops']),
+            GinIndex(fields=['channel_name'], name='music_channel_trgm', opclasses=['gin_trgm_ops'])
+        ]
+
         db_table = 'media_source'

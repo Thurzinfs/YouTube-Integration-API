d5e1a6ad7e6c1a1f2d118d1096bdca5a4f8b3ba6
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-12 18:53:05 -0300

    feat: adiciona entidade referente a track

--- app/playlist/domain/entities.py
+++ app/playlist/domain/entities.py
@@ -25,3 +25,24 @@ class PlaylistEntity:
 
         self.name = new_name
     
+
+@dataclass
+class TrackEntity:
+    id: UUID = field(default_factory=uuid4)
+    user: UUID | None = field(default=None)
+    media_source: UUID | None = field(default=None)
+    custom_title: str = field(default='')
+    created_at: datetime = field(default_factory=datetime.now)
+    deleted_at: datetime | None = field(default=None)
+
+    def deactive(self):
+        if self.deleted_at is not None:
+            ...
+
+        self.deleted_at = datetime.now()
+
+    def change_custom_title(self, new_title: str):
+        if not new_title:
+            ...
+
+        self.custom_title = new_title

a032075449f752a39d04f8c00a2af46729d4425e
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-12 18:52:35 -0300

    feat: implementa metodos para desativacao e atualizacao dos campos de playlist

--- app/playlist/domain/entities.py
+++ app/playlist/domain/entities.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass, field
+from datetime import datetime
 from uuid import UUID, uuid4
+
+from app.playlist.domain.exceptions import ConflictFieldException, FieldRequiredPlaylistException
 
 
 @dataclass
@@ -7,3 +10,18 @@ class PlaylistEntity:
     id: UUID = field(default_factory=uuid4)
     name: str = field(default='')
     user: UUID | None = field(default=None)
+    created_at: datetime = field(default_factory=datetime.now)
+    deleted_at: datetime | None = field(default=None)
+
+    def deactive(self):
+        if self.deleted_at is not None:
+            raise ConflictFieldException('playlist already deleted')
+        
+        self.deleted_at = datetime.now()
+
+    def change_name(self, new_name: str):
+        if not new_name:
+            raise FieldRequiredPlaylistException('field name is required')
+
+        self.name = new_name
+    

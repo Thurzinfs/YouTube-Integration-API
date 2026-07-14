c01f2a9cdfa692ca9e786fb4b1d810b8fca987d4
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-14 18:37:30 -0300

    feat: adiciona interface de playlist repository

---
+++ app/playlist/domain/repositories.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from uuid import UUID
+
+from app.playlist.domain.entities import PlaylistEntity, PlaylistTrackEntity, TrackEntity
+
+
+class IPlaylistRepository(ABC):
+    @abstractmethod
+    def save(self, playlist: PlaylistEntity) -> PlaylistEntity:
+        ...
+
+    @abstractmethod
+    def find_by_id(self, id: UUID) -> PlaylistEntity | None:
+        ...
+
+    @abstractmethod
+    def find_by_name(self, name: str) -> PlaylistEntity | None:
+        ...
+
+    @abstractmethod
+    def verify_exists_playlist_by_name(self, name: str) -> bool:
+        ...

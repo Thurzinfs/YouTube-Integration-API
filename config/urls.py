851ac6d4cad48c6b908d03333ae62871a4226082
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-27 19:50:32 -0300

    feat: adiciona as rotas de views.py na aplicacao

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from ninja import NinjaAPI
 
 from app.accounts.api.views import router as account_router
 from app.music.api.views import router as music_router
+from app.playlist.api.views import router_playlist, track_router, playlist_track_router
 
 api = NinjaAPI(title='Music YouTube API', docs_url='/docs/')
 
@@ -16,5 +17,8 @@ def health_check(request):
 
 api.add_router('/account', account_router, tags=['Accounts'])
 api.add_router('/music', music_router, tags=['Music'])
+api.add_router('/playlist', router_playlist, tags=['Playlist'])
+api.add_router('/track', track_router, tags=['Track'])
+api.add_router('/playlist/track', playlist_track_router, tags=['Playlist Track'])
 
 urlpatterns = [path('admin/', admin.site.urls), path('api/v1/', api.urls)]

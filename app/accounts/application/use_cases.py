9395fa288795c3c3c5a19a25a76c7f13db739cd6
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-27 23:17:39 -0300

    feat: adiciona o use case referente ao login

--- app/accounts/application/use_cases.py
+++ app/accounts/application/use_cases.py
@@ -1,13 +1,13 @@
 from uuid import UUID
 
-from app.accounts.application.dto import UserInDTO, UserOutDTO, UserUpdateDTO
+from app.accounts.application.dto import LoginInDTO, LoginOutDTO, UserInDTO, UserOutDTO, UserUpdateDTO
 from app.accounts.domain.entities import UserEntity
 from app.accounts.domain.exceptions import (
     ConflictFieldException,
     UserNotFoundException,
 )
-from app.accounts.domain.repositories import IUserRepository
-from app.accounts.domain.servicies import IHashService
+from app.accounts.domain.repositories import IRefreshTokenRepository, IUserRepository
+from app.accounts.domain.servicies import IHashService, IRefreshTokenService
 
 
 class RegisterUserUseCase:
@@ -105,3 +105,36 @@ class DeactiveUserUseCase:
 
         self.user_repo.save(user)
         return UserOutDTO.from_domain(user)
+
+
+class LoginUseCase:
+    def __init__(
+        self,
+        user_repo: IUserRepository,
+        token_repo: IRefreshTokenRepository,
+        token_service: IRefreshTokenService,
+        hash_service: IHashService,
+    ) -> None:
+        self.user_repo = user_repo
+        self.token_repo = token_repo
+        self.token_service = token_service
+        self.hash_service = hash_service
+
+    def execute(self, dto: LoginInDTO):
+        user = self.user_repo.find_by_email(dto.email)
+        if not user:
+            raise UserNotFoundException('user not found')
+
+        if user.deleted_at is not None:
+            raise UserNotFoundException('user inative')
+
+        if not self.hash_service.verify(dto.password, user.password):
+            raise UserNotFoundException('invalid credentials')
+
+        access_token = self.token_service.generate_access_token(user)
+        (raw_refresh, entity) = self.token_service.generate_refresh_token(user)
+        self.token_repo.save(entity)
+
+        return LoginOutDTO(
+            access_token=access_token, refresh_token=raw_refresh
+        )

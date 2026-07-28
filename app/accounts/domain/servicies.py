f8b90ed67dd9334d8129a4333a9b2d4a13087bd2
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-27 23:01:17 -0300

    feat: adiciona servicies referente a abtracao do service responsavel pela geracao dos tokens

--- app/accounts/domain/servicies.py
+++ app/accounts/domain/servicies.py
@@ -1,4 +1,7 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from typing import Tuple
+
+from app.accounts.domain.entities import UserEntity
 
 
 class IHashService(ABC):
@@ -6,4 +9,14 @@ class IHashService(ABC):
         ...
 
     def verify(self, raw_password: str, hash_password: str) -> bool:
+        ...
+
+
+class IRefreshTokenService(ABC):
+    @abstractmethod
+    def generate_access_token(self, user: UserEntity) -> str:
+        ...
+
+    @abstractmethod
+    def generate_refresh_token(self, user: UserEntity) -> Tuple[str, ...]:
         ...
